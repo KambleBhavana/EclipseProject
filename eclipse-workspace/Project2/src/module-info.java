322189871056b50dd6e01659e5e7bcef909d2a9c
Bhavana <209x1a05d1@gprec.ac.in>
2023-01-04 12:14:09 +0530

    Okay

---
+++ eclipse-workspace/Project2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author IIC LAB
+ *
+ */
+module Project2 {
+}
---
+++ eclipse-workspace/Project2/src/operations/file1.java
@@ -0,0 +1,10 @@
+package operations;
+
+public class file1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Everyone");
+	}
+
+}
